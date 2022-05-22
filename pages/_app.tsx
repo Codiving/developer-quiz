66a4d7b9d6a2424137eeb10bb7195203233fca1d
Codiving <codivingmoon@gmail.com>
2022-05-22 22:14:25 +0900

    reset css 추가

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,14 @@
+import { Global } from "@emotion/react";
 import type { AppProps } from "next/app";
+import resetCss from "src/styles/reset";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Global styles={resetCss} />
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default MyApp;
