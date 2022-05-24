d73c9758cde85059f8b764e58ef1434872799fca
Codiving <codivingmoon@gmail.com>
2022-05-24 19:24:43 +0900

    MainSlider 컴포넌트 리팩토링

--- src/components/MainSlider/MainSlider.tsx
+++ src/components/MainSlider/MainSlider.tsx
@@ -1,6 +1,9 @@
 import styled from "@emotion/styled";
 import { useCallback, useEffect, useState } from "react";
 import useInterval from "src/hooks/useInterval";
+import Typography from "../Typography";
+
+const sliderCount = 2;
 
 const Container = styled("div", {
   label: "MainSlider"
@@ -17,7 +20,7 @@ const RootSlider = styled("div", {
     width: "600vw",
     display: "flex",
     transform: `translate(-${index * 100}vw)`,
-    transition: index !== 1 ? "transform 0.5s ease-out" : ""
+    transition: index !== sliderCount - 2 ? "transform 0.5s ease-out" : ""
   };
 });
 
@@ -25,39 +28,88 @@ const Slider = styled("div", {
   label: "Slider"
 })(() => {
   return {
-    width: "100vw"
+    width: "100vw",
+    padding: "5em",
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+    flexDirection: "column"
   };
 });
 
 const MainSlider = () => {
-  const [index, setIndex] = useState(2);
+  const [index, setIndex] = useState(sliderCount - 2);
 
   const onAddIndex = useCallback(() => setIndex(prev => prev + 1), []);
 
-  useInterval(onAddIndex, 1000);
+  useInterval(onAddIndex, 3000);
 
   useEffect(() => {
-    if (index === 5)
+    if (index === sliderCount)
       setTimeout(() => {
-        setIndex(1);
+        setIndex(0);
       }, 500);
   }, [index]);
 
-  const onClick = () => {
-    setIndex(prev => prev + 1);
-  };
-
-  console.log("# index", index);
+  // const onClick = () => {
+  //   setIndex(prev => prev + 1);
+  // };
 
   return (
     <Container>
-      <RootSlider index={index} onClick={onClick}>
-        <Slider style={{ backgroundColor: "green" }}>가나다 1</Slider>
-        <Slider style={{ backgroundColor: "blue" }}>마바사 6</Slider>
-        <Slider style={{ backgroundColor: "green" }}>가나다 3</Slider>
-        <Slider style={{ backgroundColor: "blue" }}>마바사 4</Slider>
-        <Slider style={{ backgroundColor: "green" }}>가나다 5</Slider>
-        <Slider style={{ backgroundColor: "blue" }}>마바사 6</Slider>
+      <RootSlider index={index}>
+        <Slider
+          style={{
+            backgroundColor: "#e0e0ee"
+          }}
+        >
+          <Typography component="p">
+            내가 알고 있는{" "}
+            <Typography fontWeight={600} fontSize={32}>
+              개념
+            </Typography>
+            이 맞는 지 궁금하신가요?
+          </Typography>
+          <Typography component="p">
+            <Typography fontWeight={600} fontSize={32}>
+              스피드 객관식 Quiz
+            </Typography>
+            를 통해 확인해보세요.
+          </Typography>
+        </Slider>
+        <Slider style={{ backgroundColor: "#ffe579" }}>
+          <Typography component="p">
+            <Typography fontWeight={600} fontSize={32}>
+              면접 준비
+            </Typography>
+            를 하고 계신가요?
+          </Typography>
+          <Typography component="p">
+            <Typography fontWeight={600} fontSize={32}>
+              예상 면접 질문
+            </Typography>
+            으로{" "}
+            <Typography fontWeight={600} fontSize={32}>
+              가상 면접
+            </Typography>
+            을 진행해보세요.
+          </Typography>
+        </Slider>
+        <Slider style={{ backgroundColor: "#e0e0ee" }}>
+          <Typography component="p">
+            내가 알고 있는{" "}
+            <Typography fontWeight={600} fontSize={32}>
+              개념
+            </Typography>
+            이 맞는 지 궁금하신가요?
+          </Typography>
+          <Typography component="p">
+            <Typography fontWeight={600} fontSize={32}>
+              스피드 객관식 Quiz
+            </Typography>
+            를 통해 확인해보세요.
+          </Typography>
+        </Slider>
       </RootSlider>
     </Container>
   );
