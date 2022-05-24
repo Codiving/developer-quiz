d40b2aefff52fe57852563ea9d00bd68e975d6f5
Codiving <codivingmoon@gmail.com>
2022-05-24 19:39:07 +0900

    hexToRgbWithA util 함수
    hex color -> rgba 변환 함수

---
+++ src/utils.ts
@@ -0,0 +1,18 @@
+export const hexToRgbWithA = (hex: string, a: number) => {
+  let c;
+  if (/^#([A-Fa-f0-9]{3}){1,2}$/.test(hex)) {
+    c = hex.substring(1).split("");
+    if (c.length == 3) {
+      c = [c[0], c[0], c[1], c[1], c[2], c[2]];
+    }
+    c = "0x" + c.join("");
+    return (
+      "rgba(" +
+      [(Number(c) >> 16) & 255, (Number(c) >> 8) & 255, Number(c) & 255].join(
+        ","
+      ) +
+      `,${a})`
+    );
+  }
+  throw new Error("Bad Hex");
+};
