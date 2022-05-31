2a4efa229199310f62966195513ccedc95ae9a27
Codiving <codivingmoon@gmail.com>
2022-05-31 19:52:32 +0900

    Quiz 컴포넌트 추가

---
+++ src/components/Quiz/Quiz.tsx
@@ -0,0 +1,78 @@
+import { keyframes } from "@emotion/react";
+import styled from "@emotion/styled";
+import { useCallback, useEffect, useState } from "react";
+import { createPortal } from "react-dom";
+
+const topToBottom = keyframes` 
+  from {
+      opacity: 1;
+      transform: translate3d(0, -100%, 0);
+  }
+  to {
+      transform: translate3d(0, 0, 0);
+  }
+`;
+
+const bottomToUp = keyframes` 
+  from {
+      opacity: 1;
+      transform: translate3d(0, 0%, 0);
+  }
+  to {
+      opacity: 0;
+      transform: translate3d(0, 100%, 0);
+  }
+`;
+
+interface QuizProps {
+  onCloseAfter: () => void;
+  onCloseBefore: () => void;
+}
+
+const Container = styled("div")<{ unmount: boolean }>(({ unmount }) => {
+  return {
+    position: "fixed",
+    width: "100%",
+    height: "100%",
+    left: 0,
+    top: 0,
+    backgroundColor: "gray",
+    animation: `${unmount ? bottomToUp : topToBottom} 0.5s`
+  };
+});
+
+const Quiz = (props: QuizProps) => {
+  const { onCloseAfter, onCloseBefore } = props;
+  const [unmount, setUnmount] = useState(false);
+
+  const body = document.querySelector("body");
+  const nextId = document.querySelector("#__next");
+
+  const onClick = useCallback(() => {
+    setUnmount(true);
+    onCloseBefore();
+    setTimeout(onCloseAfter, 500);
+  }, [onCloseAfter, onCloseBefore]);
+
+  useEffect(() => {
+    if (!body) return;
+    if (!nextId) return;
+
+    setTimeout(() => nextId.classList.add("fixed-height"), 0);
+
+    return () => {
+      nextId.classList.remove("fixed-height");
+    };
+  }, [body, nextId]);
+
+  if (!body) return <>Bug</>;
+
+  return createPortal(
+    <Container unmount={unmount} onClick={onClick}>
+      Quiz?
+    </Container>,
+    body
+  );
+};
+
+export default Quiz;
