7ffef14cd24091a355c97d1ca4f42c1ffc69fc9d
Codiving <codivingmoon@gmail.com>
2022-06-01 13:05:20 +0900

    SpeedQuiz에 사용되는 공통 데이터 추가

---
+++ src/components/QuizContainer/components/SpeedQuiz/common.ts
@@ -0,0 +1,85 @@
+import { DiJavascript } from "react-icons/di";
+import { FaReact } from "react-icons/fa";
+import { GoBrowser } from "react-icons/go";
+import { SiTypescript } from "react-icons/si";
+
+export const prefix = [
+  "A",
+  "B",
+  "C",
+  "D",
+  "E",
+  "F",
+  "G",
+  "H",
+  "I",
+  "J",
+  "K",
+  "L",
+  "M",
+  "N",
+  "O",
+  "P",
+  "Q",
+  "R",
+  "S",
+  "T",
+  "U",
+  "V",
+  "W",
+  "X",
+  "Y",
+  "Z"
+];
+
+export const SPEED_QUIZ_CATEGORY = {
+  Web: "Web",
+  JavaScript: "JavaScript",
+  TypeScript: "TypeScript",
+  React: "React"
+} as const;
+
+export type SpeedQuizCategory =
+  typeof SPEED_QUIZ_CATEGORY[keyof typeof SPEED_QUIZ_CATEGORY];
+
+const commonIconStyle = {
+  width: 24,
+  height: 24
+};
+
+export const SPEED_QUIZ_CATEGORY_DATA = {
+  [SPEED_QUIZ_CATEGORY.Web]: {
+    color: "#4b4bcd",
+    iconStyle: {
+      iconColor: "#4b4bcd",
+      ...commonIconStyle
+    },
+    startIcon: GoBrowser
+  },
+  [SPEED_QUIZ_CATEGORY.JavaScript]: {
+    color: "#ff9800",
+    iconStyle: {
+      iconColor: "#ff9800",
+      ...commonIconStyle
+    },
+
+    startIcon: DiJavascript
+  },
+  [SPEED_QUIZ_CATEGORY.TypeScript]: {
+    color: "#2f74c0",
+    iconStyle: {
+      iconColor: "#2f74c0",
+      ...commonIconStyle
+    },
+    startIcon: SiTypescript
+  },
+  [SPEED_QUIZ_CATEGORY.React]: {
+    color: "#03a9f4",
+    iconStyle: {
+      iconColor: "#03a9f4",
+      ...commonIconStyle
+    },
+
+    startIcon: FaReact
+  }
+};
