906da63b864567d6e1870184b1a883d661716885
Codiving <codivingmoon@gmail.com>
2022-06-01 11:36:16 +0900

    MainDescription 리팩토링
    데이터 분리

--- src/components/MainDescription/MainDescription.tsx
+++ src/components/MainDescription/MainDescription.tsx
@@ -2,6 +2,7 @@ import styled from "@emotion/styled";
 import React, { useCallback, useEffect, useState } from "react";
 import QuizContainer from "../QuizContainer";
 import { Description } from "./components";
+import { DescriptionData } from "./data";
 import { QUIZ_CATEGORY } from "./types";
 
 const Container = styled("section", {
@@ -41,33 +42,19 @@ const MainDescription = () => {
         </QuizContainer>
       )}
       <Container>
-        <Description
-          title="스피드 객관식 퀴즈 !"
-          texts={[
-            "내가 어떠한 개념을 모르고 있는 지 궁금한 경우가 있습니다.",
-            "테스트 하고 싶은 카테고리만 골라 객관식 문제를 풀어봅시다."
-          ]}
-          image="https://assets.awwwards.com/awards/media/cache/thumb_417_299/submissions/2022/05/6284ca3a3785e878211698.jpg"
-          alt="이미지"
-          buttonText="퀴즈 풀어보기"
-          category={QUIZ_CATEGORY[0]}
-          selectedCagtegory={selectedCagtegory}
-          onSelectedCategory={onSelectedCategory}
-        />
-        <Description
-          title="가상 면접"
-          texts={[
-            "취업 면접 시 나올만한 질문들을 모았습니다.",
-            "모르는 내용인 경우 추천 키워드를 통해 공부합시다."
-          ]}
-          image="https://assets.awwwards.com/awards/media/cache/thumb_417_299/submissions/2022/05/6284ca3a3785e878211698.jpg"
-          alt="이미지"
-          isImageRight={false}
-          buttonText="가상 면접보기"
-          category={QUIZ_CATEGORY[1]}
-          selectedCagtegory={selectedCagtegory}
-          onSelectedCategory={onSelectedCategory}
-        />
+        {QUIZ_CATEGORY.map((category, index) => {
+          const data = DescriptionData.get(category);
+          return (
+            <Description
+              key={category}
+              isImageRight={!Boolean(index % 2)}
+              category={category}
+              selectedCagtegory={selectedCagtegory}
+              onSelectedCategory={onSelectedCategory}
+              {...data}
+            />
+          );
+        })}
       </Container>
     </>
   );
---
+++ src/components/MainDescription/data.ts
@@ -0,0 +1,34 @@
+import { QUIZ_CATEGORY } from "./types";
+
+const data = [
+  {
+    id: QUIZ_CATEGORY[0],
+    title: "스피드 객관식 퀴즈 !",
+    texts: [
+      "내가 어떠한 개념을 모르고 있는 지 궁금한 경우가 있습니다.",
+      "테스트 하고 싶은 카테고리만 골라 객관식 문제를 풀어봅시다."
+    ],
+    image:
+      "https://assets.awwwards.com/awards/media/cache/thumb_417_299/submissions/2022/05/6284ca3a3785e878211698.jpg",
+    alt: "이미지",
+    buttonText: "퀴즈 풀어보기"
+  },
+  {
+    id: QUIZ_CATEGORY[1],
+    title: "가상 면접",
+    texts: [
+      "취업 면접 시 나올만한 질문들을 모았습니다.",
+      "모르는 내용인 경우 추천 키워드를 통해 공부합시다."
+    ],
+    image:
+      "https://assets.awwwards.com/awards/media/cache/thumb_417_299/submissions/2022/05/6284ca3a3785e878211698.jpg",
+    alt: "이미지",
+    buttonText: "가상 면접보기"
+  }
+];
+
+export const DescriptionData = new Map();
+
+data.forEach(item => {
+  DescriptionData.set(item.id, item);
+});
