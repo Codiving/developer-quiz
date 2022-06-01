fd006220f24d7df41f00ac77f5a8149b660787b5
Codiving <codivingmoon@gmail.com>
2022-06-01 13:06:54 +0900

    SpeedQuiz 컴포넌트 구현 중

--- src/components/QuizContainer/components/SpeedQuiz/SpeedQuiz.tsx
+++ src/components/QuizContainer/components/SpeedQuiz/SpeedQuiz.tsx
@@ -1,5 +1,21 @@
+import styled from "@emotion/styled";
+import { SpeedQuizGenerator, SpeedQuizList } from "./components";
+
+const Container = styled("div", { label: "SpeedQuiz" })(() => {
+  return {
+    height: "100%",
+    display: "grid",
+    gridTemplateColumns: "1fr 1.5fr"
+  };
+});
+
 const SpeedQuiz = () => {
-  return <div></div>;
+  return (
+    <Container>
+      <SpeedQuizGenerator />
+      <SpeedQuizList />
+    </Container>
+  );
 };
 
 export default SpeedQuiz;
