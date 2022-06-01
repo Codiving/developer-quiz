99023fb1eff57efcb68667a82e59b6c4e80f3afe
Codiving <codivingmoon@gmail.com>
2022-06-01 21:24:15 +0900

    SpeedQuiz CSS 업데이트

--- src/components/QuizContainer/components/SpeedQuiz/SpeedQuiz.tsx
+++ src/components/QuizContainer/components/SpeedQuiz/SpeedQuiz.tsx
@@ -8,7 +8,7 @@ const Container = styled("div", { label: "SpeedQuiz" })(() => {
   return {
     height: "100%",
     display: "grid",
-    gridTemplateColumns: "1fr 1fr"
+    gridTemplateColumns: "minmax(480px, 0.6fr) 1fr"
   };
 });
 
