1c2c638aa4a521ae7fb7aa2c9abea213d5c2b878
Codiving <codivingmoon@gmail.com>
2022-06-19 16:20:51 +0900

    ThemeProvider 추가

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,15 +2,16 @@ import { Global } from "@emotion/react";
 import type { AppProps } from "next/app";
 import { Footer, Header } from "src/layout";
 import resetCss from "src/styles/reset";
+import ThemeProvider from "styles/src/context/ThemeContext";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <>
+    <ThemeProvider>
       <Global styles={resetCss} />
       <Header />
       <Component {...pageProps} />
       <Footer />
-    </>
+    </ThemeProvider>
   );
 }
 
